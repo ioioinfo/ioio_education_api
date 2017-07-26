15c42f42238eb9ce1144ca8a1f0edb8acd74d1d5
apple2@ioioinfo.com <apple2@ioioinfo.com>
2017-07-26 17:02:15 +0800

    update

--- server/controller/lessons_controller.js
+++ server/controller/lessons_controller.js
@@ -40,6 +40,86 @@ var do_result = function(err,result,cb){
 exports.register = function(server, options, next) {
 
     server.route([
+		//查询班级
+        {
+            method: "GET",
+            path: '/get_lessons',
+            handler: function(request, reply) {
+				var params = request.query.params;
+                var info = {};
+                if (params) {
+                    info = JSON.parse(params);
+                }
+                var ep =  eventproxy.create("rows", "plans", "teachers", "grades", "num",
+					function(rows, plans, teachers, grades, num){
+                        for (var i = 0; i < rows.length; i++) {
+                            var row = rows[i];
+                            if (plans[row.plan_id]) {
+                                row.plan = plans[row.plan_id];
+                            }
+                            if (teachers[row.teacher_id]) {
+                                row.teacher = teachers[row.teacher_id];
+                            }
+                            if (grades[row.level_id]) {
+                                row.level = grades[row.level_id];
+                            }
+                        }
+					return reply({"success":true,"rows":rows,"num":num,"service_info":service_info});
+				});
+                //查询所有课程
+                server.plugins['models'].lessons.get_lessons(info,function(err,rows){
+                    if (!err) {
+						ep.emit("rows", rows);
+					}else {
+						ep.emit("rows", []);
+					}
+				});
+				server.plugins['models'].lessons.account_lessons(info,function(err,rows){
+                    if (!err) {
+						ep.emit("num", rows[0].num);
+					}else {
+						ep.emit("num", 0);
+					}
+				});
+                //查询所有计划
+                server.plugins['models'].lesson_plans.get_lesson_plans(function(err,rows){
+                    if (!err) {
+                        var plans_map = {};
+						for (var i = 0; i < rows.length; i++) {
+							plans_map[rows[i].id] = rows[i];
+						}
+						ep.emit("plans", plans_map);
+					}else {
+						ep.emit("plans", {});
+					}
+				});
+                //查询所有老师
+                server.plugins['models'].teachers.get_teachers(function(err,rows){
+                    if (!err) {
+                        var teachers_map = {};
+						for (var i = 0; i < rows.length; i++) {
+							teachers_map[rows[i].id] = rows[i];
+						}
+						ep.emit("teachers", teachers_map);
+					}else {
+						ep.emit("teachers", {});
+					}
+				});
+                //查询所有年级
+                server.plugins['models'].grade_levels.get_grades(function(err,rows){
+                    if (!err) {
+                        var grades_map = {};
+                        for (var i = 0; i < rows.length; i++) {
+                            grades_map[rows[i].id] = rows[i];
+                        }
+                        ep.emit("grades", grades_map);
+                    }else {
+                        ep.emit("grades", {});
+                    }
+                });
+            }
+        },
+
 
     ]);
 
--- server/models/lessons.js
+++ server/models/lessons.js
@@ -3,6 +3,52 @@ var EventProxy = require('eventproxy');
 
 var lessons = function(server) {
 	return {
+		//获得所有班级
+		get_lessons : function(info, cb){
+            var query = `select id, plan_id, teacher_id, name, code, hours,
+			level_id, created_at, updated_at
+            from lessons where flag = 0
+            `;
+
+			// if (info.thisPage) {
+            //     var offset = info.thisPage-1;
+            //     if (info.everyNum) {
+            //         query = query + " limit " + offset*info.everyNum + "," + info.everyNum;
+            //     }else {
+            //         query = query + " limit " + offset*20 + ",20";
+            //     }
+            // }
+            server.plugins['mysql'].query(query, function(err, results) {
+                if (err) {
+                    console.log(err);
+                    cb(true,results);
+                    return;
+                }
+                cb(false,results);
+            });
+        },
+		account_lessons : function(info, cb){
+			var query = `select count(1) num
+			from lessons where flag = 0
+			`;
+
+			if (info.thisPage) {
+				var offset = info.thisPage-1;
+				if (info.everyNum) {
+					query = query + " limit " + offset*info.everyNum + "," + info.everyNum;
+				}else {
+					query = query + " limit " + offset*20 + ",20";
+				}
+			}
+			server.plugins['mysql'].query(query, function(err, results) {
+				if (err) {
+					console.log(err);
+					cb(true,results);
+					return;
+				}
+				cb(false,results);
+			});
+		},
 
 
 	};
